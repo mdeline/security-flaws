e676404cc71975cf3c91121e3d5a3c8413c9b14d
Ella <ellamadelineberg@gmail.com>
2021-12-26 11:43:54 +0200

    Initialize Spring Boot backend

---
+++ backend/src/test/java/fi/mdeline/securityflaws/SecurityflawsApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.mdeline.securityflaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityflawsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
