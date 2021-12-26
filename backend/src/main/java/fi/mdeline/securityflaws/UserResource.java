e21273457b7dabc73785b0fa02aaaa8996ba5c39
Ella <ellamadelineberg@gmail.com>
2021-12-26 22:02:24 +0200

    Add API points

---
+++ backend/src/main/java/fi/mdeline/securityflaws/UserResource.java
@@ -0,0 +1,38 @@
+package fi.mdeline.securityflaws;
+
+import fi.mdeline.securityflaws.model.User;
+import fi.mdeline.securityflaws.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserResource {
+    public final UserService userService;
+
+    public UserResource(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<User>> getUsers() {
+        List<User> users = userService.findAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable("id") Long id) {
+        User user = userService.findUserById(id);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<User> addUser(@RequestBody User user) {
+        User newUser = userService.addUser(user);
+        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+    }
+
+}
