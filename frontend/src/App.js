3fde7b021b046c27597d2b045718bbd5bdcc1472
Ella <ellamadelineberg@gmail.com>
2021-12-27 19:10:45 +0200

    Add register component and routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,22 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 import './App.css';
-import Users from './components/Users';
+import { Link, Outlet } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <h1>Security Flaws App</h1>
-      <Users />
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem"
+        }}
+      >
+        <Link to="/">Home</Link> | {" "}
+        <Link to="/register">Register</Link> | {" "}
+        <Link to="/users">Users</Link>
+      </nav>
+      <Outlet />
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,21 @@
 import App from './App';
+import Register from './components/Register';
+import Users from './components/Users';
 import { render } from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 
 render(
   <BrowserRouter>
-    <App />
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="register" element={<Register />} />
+        <Route path="users" element={<Users />} />
+      </Route>
+    </Routes>
   </BrowserRouter>,
   document.getElementById('root')
 );
