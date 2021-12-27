39b67108904c5d0c964f873a4de6b34bd3449b6c
Ella <ellamadelineberg@gmail.com>
2021-12-27 12:31:51 +0200

    Fetch users from API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 import axios from "axios";
 
+const Users = () =>  {
+  const fetchUsers = () => {
+    axios.get("http://localhost:8080/users/all")
+    .then(response => {
+      console.log(response);
+    })
+  }
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  return <h1>Hello</h1>
+}
+
 function App() {
   return (
     <div className="App">
+      <Users />
       <p>Hello</p>
     </div>
   );
