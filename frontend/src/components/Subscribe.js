d397577d234e6248289e5d392e3a3273b6d4b7d9
Ella <ellamadelineberg@gmail.com>
2021-12-28 16:55:43 +0200

    Send post

--- frontend/src/components/Subscribe.js
+++ frontend/src/components/Subscribe.js
@@ -1,20 +1,33 @@
-import * as React from "react";
+import React, { useState } from 'react';
 import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
 
 function Subscribe() {
-  const { register, handleSubmit, formState: { errors } } = useForm();
-  const onSubmit = handleSubmit(data => console.log(data));
+  const [message, setMessage] = useState('');
+  const { register, handleSubmit, formState: { errors }, reset } = useForm();
+  const onSubmit = handleSubmit(
+    data => {
+      console.log(data);
+      axios.post("http://localhost:8080/subscribers/add", data);
+      setMessage(`Thank you for subscribing.`);
+      reset();
+    });
   return (
     <div>
     <h2>Subscribe to our newsletter</h2>
+   
     <form onSubmit={onSubmit}>
       <label>Name</label> <br />
       <input type="text" {...register("name", { required: true})} /> <br />
       <label>E-mail</label> <br />
       <input type="email" {...register("email", {required: true})} /> <br />
       <button type="submit">Submit</button>
-
     </form>
+
+    <h3>
+      { message }
+    </h3>
+
     </div>
   );
 }
