4f620d5931c08da4d6384f3ed339a1769eec65e1
Ella <ellamadelineberg@gmail.com>
2021-12-29 14:52:18 +0200

    Remove the old backend

--- backend/src/main/java/fi/mdeline/securityflaws/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package fi.mdeline.securityflaws.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public  UserNotFoundException(String message) {
-        super(message);
-    }
-}
